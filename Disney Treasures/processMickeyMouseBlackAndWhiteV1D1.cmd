9bb72c7c5fff1f64405008465e629ecafacb90f9
matthiasxc <matthias.shapiro@gmail.com>
2019-01-23 11:18:18 -0800

    updates to align Mickey Mouse in Black and White Vol 1 and Mickey Mouse in Living Color Vol 1 with the discs as they exist (and make the script a little more generalizable

--- Disney Treasures/processMickeyMouseBlackAndWhiteV1D1.cmd
+++ Disney Treasures/processMickeyMouseBlackAndWhiteV1D1.cmd
@@ -2,40 +2,40 @@ rem Renaming and organizing for the video from
 rem Mickey Mouse in Black and White, Volume 1 Disc 1
 
 rem rename all the files
-ren "Title00*" "Disney Animated Shorts - S1931E01 - The Birthday Party.*"
-ren "Title01*" "Disney Animated Shorts - S1931E12 - Blue Rhythm.*"
-ren "Title02*" "Disney Animated Shorts - S1930E15 - The Chain Gang.*"
-ren "Title03*" "Disney Animated Shorts - S1932E02 - The Duck Hunt.*"
-ren "Title04*" "Disney Animated Shorts - S1930E13 - The Fire Fighters.*"
+ren "*00.*" "Disney Animated Shorts - S1931E01 - The Birthday Party.*"
+ren "*01.*" "Disney Animated Shorts - S1931E12 - Blue Rhythm.*"
+ren "*02.*" "Disney Animated Shorts - S1930E15 - The Chain Gang.*"
+ren "*03.*" "Disney Animated Shorts - S1932E02 - The Duck Hunt.*"
+ren "*04.*" "Disney Animated Shorts - S1930E13 - The Fire Fighters.*"
 rem This one has the annoying Matlin commentary
-ren "Title05*" "Disney Animated Shorts - S1928E17 - The Gallopin Gaucho.*"
-ren "Title06*" "Disney Animated Shorts - S1930E17 - The Gorilla Mystery.*"
-ren "Title07*" "Disney Animated Shorts - S1929S05 - The Karnival Kid.*"
+ren "*05.*" "Disney Animated Shorts - S1928E17 - The Gallopin Gaucho.*"
+ren "*06.*" "Disney Animated Shorts - S1930E17 - The Gorilla Mystery.*"
+ren "*07.*" "Disney Animated Shorts - S1929S05 - The Karnival Kid.*"
 rem This one has the annoying Matlin commentary
-ren "Title08*" "Disney Animated Shorts - S1932E19 - The Klondike Kid.*"
-ren "Title09*" "Disney Animated Shorts - S1931E20 - Mickey Cuts Up.*"
+ren "*08.*" "Disney Animated Shorts - S1932E19 - The Klondike Kid.*"
+ren "*09.*" "Disney Animated Shorts - S1931E20 - Mickey Cuts Up.*"
 rem This one has the annoying Matlin commentary 
-ren "Title10*" "Disney Animated Shorts - S1931E10 - Mickey Steps Out.*"
+ren "*10.*" "Disney Animated Shorts - S1931E10 - Mickey Steps Out.*"
 
-ren "Title11*" "Disney Animated Shorts - S1929E07 - Mickey's Follies.*"
-ren "Title12*" "Disney Animated Shorts - S1932E12 - Mickey's Nightmare.*"
-ren "Title13*" "Disney Animated Shorts - S1931E21 - Mickey's Orphans.*"
-ren "Title14*" "Disney Animated Shorts - S1932E06 - Mickey's Revue.*"
+ren "*11.*" "Disney Animated Shorts - S1929E07 - Mickey's Follies.*"
+ren "*12.*" "Disney Animated Shorts - S1932E12 - Mickey's Nightmare.*"
+ren "*13.*" "Disney Animated Shorts - S1931E21 - Mickey's Orphans.*"
+ren "*14.*" "Disney Animated Shorts - S1932E06 - Mickey's Revue.*"
 rem This one has the annoying Matlin commentary 
-ren "Title15*" "Disney Animated Shorts - S1930E20 - Pioneer Days.*"
-ren "Title16*" "Disney Animated Shorts - S1928E11 - Plane Crazy.*"
-ren "Title17*" "Disney Animated Shorts - S1928E20 - Steamboat Willie.*"
-ren "Title18*" "Disney Animated Shorts - S1932E17 - Touchdown Mickey.*"
-ren "Title19*" "Disney Animated Shorts - S1932E15 - The Whoopee Party.*"
+ren "*15.*" "Disney Animated Shorts - S1930E20 - Pioneer Days.*"
+ren "*16.*" "Disney Animated Shorts - S1928E11 - Plane Crazy.*"
+ren "*17.*" "Disney Animated Shorts - S1928E20 - Steamboat Willie.*"
+ren "*18.*" "Disney Animated Shorts - S1932E17 - Touchdown Mickey.*"
+ren "*19.*" "Disney Animated Shorts - S1932E15 - The Whoopee Party.*"
 
-ren "Title20*" "Disney Animated Shorts - The Origin of Mickey Mouse.*"
-ren "Title21*" "Disney Animated Shorts - Frank and Ollie and Mickey.*"
-ren "Title22*" "Disney Animated Shorts - The Mickey Mouse Club.*"
-ren "Title23*" "Disney Animated Shorts - Story Sketches 01.*"
-ren "Title24*" "Disney Animated Shorts - Story Sketches 02.*"
-ren "Title25*" "Disney Animated Shorts - Story Sketches 03.*"
-ren "Title26*" "Disney Animated Shorts - Story Sketches 04.*"
-ren "Title27*" "Disney Animated Shorts - Story Sketches 05.*"
+ren "*20.*" "Disney Animated Shorts - The Origin of Mickey Mouse.*"
+ren "*21.*" "Disney Animated Shorts - Frank and Ollie and Mickey.*"
+ren "*22.*" "Disney Animated Shorts - The Mickey Mouse Club.*"
+ren "*23.*" "Disney Animated Shorts - Story Sketches 01.*"
+ren "*24.*" "Disney Animated Shorts - Story Sketches 02.*"
+ren "*25.*" "Disney Animated Shorts - Story Sketches 03.*"
+ren "*26.*" "Disney Animated Shorts - Story Sketches 04.*"
+ren "*27.*" "Disney Animated Shorts - Story Sketches 05.*"
 
 rem create all the folders we need and move the files into them
 mkdir 1928
--- Disney Treasures/processMickeyMouseLivingColorV1D2.cmd
+++ Disney Treasures/processMickeyMouseLivingColorV1D2.cmd
@@ -2,22 +2,21 @@ rem Renaming and organizing for the video from
 rem The Donald Duck download
 
 rem rename all the files
-ren "Title00*" "Disney Animated Shorts - S1937E09 - Hawaiian Holiday.*"
-ren "Title01*" "Disney Animated Shorts - S1937E04 - Moose Hunters.*"
-ren "Title02*" "Disney Animated Shorts - S1937E01 - The Worm Turns.*"
-ren "Title03*" "Disney Animated Shorts - S1937E03 - Magician Mickey.*"
-ren "Title04*" "Disney Animated Shorts - S1937E06 - Mickey's Amateurs.*"
-ren "Title05*" "Disney Animated Shorts - S1937E10 - Clock Cleaners.*"
-ren "Title06*" "Disney Animated Shorts - S1937E14 - Lonesome Ghosts.*"
-ren "Title07*" "Disney Animated Shorts - S1938E12 - Mickey's Parrot.*"
-ren "Title08*" "Disney Animated Shorts - S1938E02 - Boat Builders.*"
-ren "Title09*" "Disney Animated Shorts - S1938S11 - The Whalers.*"
-ren "Title10*" "Disney Animated Shorts - S1938E06 - Mickey's Trailer.*"
-ren "Title11*" "Disney Animated Shorts - S1938E13 - Brave Little Tailor.*"
+ren "*00.*" "Disney Animated Shorts - S1937E09 - Hawaiian Holiday.*"
+ren "*01.*" "Disney Animated Shorts - S1937E04 - Moose Hunters.*"
+ren "*02.*" "Disney Animated Shorts - S1937E01 - The Worm Turns.*"
+ren "*03.*" "Disney Animated Shorts - S1937E03 - Magician Mickey.*"
+ren "*04.*" "Disney Animated Shorts - S1937E06 - Mickey's Amateurs.*"
+ren "*05.*" "Disney Animated Shorts - S1937E10 - Clock Cleaners.*"
+ren "*06.*" "Disney Animated Shorts - S1937E14 - Lonesome Ghosts.*"
+ren "*07.*" "Disney Animated Shorts - S1938E12 - Mickey's Parrot.*"
+ren "*08.*" "Disney Animated Shorts - S1938E02 - Boat Builders.*"
+ren "*09.*" "Disney Animated Shorts - S1938S11 - The Whalers.*"
+ren "*10.*" "Disney Animated Shorts - S1938E06 - Mickey's Trailer.*"
+ren "*11.*" "Disney Animated Shorts - S1938E13 - Brave Little Tailor.*"
 
-ren "Title12*" "Disney Animated Shorts - All Shorts For This Disc.*"
-ren "Title13*" "Disney Animated Shorts - Leonard Maltin Talks About Mickey In Color.*"
-ren "Title14*" "Disney Animated Shorts - S1939E14 - Mickey Surprise Party.*"
+ren "*12.*" "Disney Animated Shorts - All Shorts For This Disc.*"
+ren "*13.*" "Disney Animated Shorts - Leonard Maltin Talks About Mickey In Color.*"
 
 rem create all the folders we need and move the files into them
 mkdir 1937
